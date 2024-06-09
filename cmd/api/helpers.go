4b789b798b8cbbd743530ee06127ddde5c5f0a2d
tonytheleg <anatale@redhat.com>
2024-06-09 13:03:23 -0400

    bug fix

--- cmd/api/helpers.go
+++ cmd/api/helpers.go
@@ -2,12 +2,13 @@ package main
 
 import (
 	"errors"
-	"github.com/julienschmidt/httprouter"
 	"net/http"
 	"strconv"
+
+	"github.com/julienschmidt/httprouter"
 )
 
-func (app *application) readIDParam(r *http.Request) (int64, error) {
+func (app *application) readIDParam(w http.ResponseWriter, r *http.Request) (int64, error) {
 	params := httprouter.ParamsFromContext(r.Context())
 
 	id, err := strconv.ParseInt(params.ByName("id"), 10, 64)
--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -13,7 +13,7 @@ func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Reques
 // showMovieHandler gets movies, any interpolated URL params will be stored
 // in the request context, we use ParamsFromContext to get those values
 func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request) {
-	id, err := app.readIDParam(r)
+	id, err := app.readIDParam(w, r)
 	if err != nil {
 		http.NotFound(w, r)
 		return
